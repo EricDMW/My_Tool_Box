984ca9d0591b01fb02d6fef2d52742733a27b506
dongming <wdong025@ucr.edu>
2025-06-27 20:23:04 -0700

    update all three toolkit

--- toolkit_project/setup.py
+++ toolkit_project/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="plotkit",
+    name="toolkit",
     version="0.1.0",
     description="A simple, extensible plotting toolkit for RL and research.",
     author="Dongming Wang",
